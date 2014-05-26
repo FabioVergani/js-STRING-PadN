cd855299d246d36b5e84af6f44f05d3a50e0129c
FabioVergani <fabio.vergani.1975@gmail.com>
2014-05-26 19:40:52 +0200

    Update String.padN.js

--- String.padN.js
+++ String.padN.js
@@ -1,3 +1,22 @@
+
+
+
+
+/*
+
+
+function zfill(number, size) {
+  number = number.toString();
+  while (number.length < size) number = "0" + number;
+  return number;
+}
+
+
+
 String.padN=function(s,l,t){return s.length>=l?s:String.padN(t+s,l,t)};
 
 console.log(String.padN("11",5,'0'))
+
+
+
+*/
