55496809a7874b2ff9208b8db6461b615444be1a
FabioVergani <fabio.vergani.1975@gmail.com>
2014-05-27 10:56:03 +0200

    Update String.padN.js

--- String.padN.js
+++ String.padN.js
@@ -1,11 +1,20 @@
 
 
 String.repeat=function(s,n){var t='';if(n>1){while(n>0){if(n%2===1){t+=s};s+=s;n>>=1}}else{t=n<=0?'':s};return t};
-//String.padN=function(s,i,t){return t?i?String.repeat(t,i-s.length)+s:s:s};
 
 
 String.padN=function(s,i,t){return i===0||t.length===0?s:String.repeat(t,i-s.length)+s};
 
+console.log(String.padN("1",0));
+console.log(String.padN("1",2,'-'));
+console.log(String.padN("1",1,'x'));
+console.log(String.padN("1",2,'x'));
+var test=String.padN("1",23,'x');
+console.log(test,test.length);
+
+
+
+//String.padN=function(s,i,t){return t?i?String.repeat(t,i-s.length)+s:s:s};
 
 //String.padN=function(s,l,t){while(s.length<l){s=t+s};return s}
 
