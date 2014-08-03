8918605a284f63e2419cd30e4c841298aa5f7b5e
FabioVergani <fabio.vergani.1975@gmail.com>
2014-08-03 20:28:45 +0200

    Create jPad.js

---
+++ jPad.js
@@ -0,0 +1,6 @@
+function jPad(x){var m=Math,f=String,i=['length'];return f(m.round(m.pow(10,f(m.floor(x))[i])))[i]};
+
+console.log(jPad(2));
+console.log(jPad(99));
+console.log(jPad(111.8));
+console.log(jPad(999.8));
